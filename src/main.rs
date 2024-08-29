e726330402f46f28b80805b865e1707a59ad6db8
Lucas Bombarda <lucas@devdock.com.br>
2024-08-28 21:00:53 -0300

    feat: that's it, we are counting everything
    ok, now the tool count the lines... literally everything :no_good_man:

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 use clap::Parser;
+use encoding::{all::UTF_8, DecoderTrap, Encoding};
+use std::collections::HashMap;
+use std::{error::Error, fs::File, io::prelude::*, result};
+use walkdir::WalkDir;
+
+type Result<T> = result::Result<T, Box<dyn Error>>;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about)]
@@ -8,7 +14,41 @@ struct Cli {
     path: String,
 }
 
-fn main() {
+fn count_lines(file_path: String) -> usize {
+    let mut file = File::open(file_path).unwrap();
+    let mut buff = Vec::new();
+    file.read_to_end(&mut buff).unwrap();
+    let res = UTF_8.decode(&buff, DecoderTrap::Ignore);
+    res.unwrap().lines().count()
+}
+
+fn exec(path: String) -> Result<()> {
+    println!("Counting...\n");
+    let mut res: HashMap<String, usize> = HashMap::new();
+    for entry in WalkDir::new(path)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.path().is_file())
+    {
+        let path = entry.path().display().to_string();
+        if let Some(file_type) = entry.path().extension() {
+            let lines = count_lines(path);
+            res.entry(file_type.to_str().unwrap().to_string())
+                .and_modify(|qty| *qty += lines)
+                .or_insert(lines);
+        }
+    }
+
+    println!("Total raw LOC: {}\n", res.values().sum::<usize>());
+    res.iter().for_each(|(k, v)| {
+        println!("{k}: {v}");
+    });
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
     let cli = Cli::parse();
-    println!("{:?}", cli);
+    exec(cli.path)?;
+    Ok(())
 }
